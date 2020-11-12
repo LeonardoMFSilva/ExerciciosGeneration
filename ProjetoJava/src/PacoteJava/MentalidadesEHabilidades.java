9c0b997991b9e72759034ae585379c0da2751af6
unknown <leo_leb5@hotmail.com>
2020-11-12 16:45:12 -0300

    Exercicios Java 12'11

---
+++ ProjetoJava/src/PacoteJava/MentalidadesEHabilidades.java
@@ -0,0 +1,18 @@
+package PacoteJava;
+
+public class MentalidadesEHabilidades {
+	
+	public static void main(String args[]) {
+		
+		String mentalidades = "As mentalidades que tive que usar, foram de resiliência\n"
+				+ "e crescimento. Pois são coisas que não estou acostumado e por serem\n"
+				+ "recentes, não estou acostumado a realizar. \n";
+		
+		String habilidades = "\nAs habilidaes que tive que usar para chegar onde estou, foi"
+				+ "a criatividade e persistência,\npois não acreditava que seria possível estar aqui"
+				+ "Agora, com essas pessoas incríveis e com dois instrutores fantásticos!";
+		
+		System.out.println(mentalidades + habilidades);		
+	}
+
+}
---
+++ ProjetoJava/src/PacoteJava/SobreMim.java
@@ -0,0 +1,18 @@
+package PacoteJava;
+
+
+public class SobreMim {
+	
+	public static void main(String args[])
+	{
+		String descricao = "Podem me chamar de Leeo, tenho 26 anos. \n"
+				+ "Gosto de passar tempo com a minha família, jogar video-game, ultimamente "
+				+ "estou jogando bastante Warzone e Fifa 21, \nE "
+				+ "futuramente codar (espero que em um futuro próximo). \nGosto também"
+				+ "de tocar violão e escutar música, principalmente rap, R&B e hip-hop, mas gosto"
+				+ "de outos estilos músicais também.\n"
+				+ "Viajar sozinho, com amigos ou familia também é uma das coisas que gosto muito.";
+		System.out.println(descricao);
+	}
+
+}
